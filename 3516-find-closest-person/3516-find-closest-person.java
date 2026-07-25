d601236bcb869a5dace4f9c01eb5edb16dbf582e
Pragati_Vele <pragativele@gmail.com>
2026-07-25 15:35:49 +0530

    Time: 1 ms (93.62%), Space: 42.7 MB (22.12%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int p1 = Math.abs(x - z);
+        int p2 =  Math.abs(y - z);
+    
+        if(p1 == p2){
+            return 0;
+        }else if(p1 < p2){
+            return 1;
+        }else{
+            return 2;
+        }
+    }
+}
