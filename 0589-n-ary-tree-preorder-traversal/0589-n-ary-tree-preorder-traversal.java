0d729be8b3cf7a329ab90b00e42e3c4564721aea
Pragati_Vele <pragativele@gmail.com>
2026-08-31 22:31:07 +0530

    Time: 0 ms (100%), Space: 46.6 MB (35.26%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,32 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    ArrayList<Integer> ans = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        if(root == null){
+            return new ArrayList<>();
+        }
+        ans.add(root.val);
+        for(Node curr: root.children){
+            preorder(curr);
+        }
+        return ans;
+    }
+}
