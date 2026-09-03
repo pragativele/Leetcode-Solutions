1a2115958ddf28275933a4737b44cf935fd20587
Pragati_Vele <pragativele@gmail.com>
2026-09-03 19:44:40 +0530

    Time: 2 ms (11.76%), Space: 50.2 MB (47.4%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        for(int i=0; i<citations.length; i++){
+            int h = citations.length-i;
+            if(citations[i] >= h){
+                return h;
+            }
+        }
+        return 0;
+    }
+}
